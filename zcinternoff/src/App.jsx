ffa177c8d42e4ccd1012b7e9e6957e9a0db54091
mdfaizan973 <faizan.md9735@gmail.com>
2023-11-14 15:01:12 +0530

    started Project

---
+++ zcinternoff/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Hello - world</h1>
+      <p>I am gonna create a very good website using react and tailwind CSS.</p>
+    </div>
+  );
+}
